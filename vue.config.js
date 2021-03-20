b1650c951e3d2100683c17e6448616d00d0ce47c
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2021-03-20 12:19:37 +0100

    fix github pages deployment for real

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   // GH pages are served from a sub-path, which we have to accomodate for
-  publicPath: process.env.NODE_ENV === 'production' ? '/' + process.env.CI_PROJECT_NAME + '/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/FinderEditor/' : '/',
   lintOnSave: false,
 };
