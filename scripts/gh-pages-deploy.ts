ee08ae435f54cc2285f5267e80d8305d33782db1
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2021-03-20 11:43:46 +0100

    add gh-pages deploy script

---
+++ scripts/gh-pages-deploy.ts
@@ -0,0 +1,22 @@
+const execa = require('execa');
+
+(async () => {
+  try {
+    await execa('git', ['checkout', '--orphan', 'gh-pages']);
+    console.log('Building...');
+    await execa('npm', ['run', 'build']);
+    // Understand if it's dist or build folder
+    const folderName = 'dist';
+    await execa('git', ['--work-tree', folderName, 'add', '--all']);
+    await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages']);
+    console.log('Pushing to gh-pages...');
+    await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force']);
+    await execa('rm', ['-r', folderName]);
+    await execa('git', ['checkout', '-f', 'main']);
+    await execa('git', ['branch', '-D', 'gh-pages']);
+    console.log('Successfully deployed');
+  } catch (e) {
+    console.log(e.message);
+    process.exit(1);
+  }
+})();
