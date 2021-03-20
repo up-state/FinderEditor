2a574b9fb0aceaa3c4d48b864d41c38eeb13b0ea
Dennis Breitling <breitling@posteo.de>
2021-03-20 12:15:10 +0100

    remove unused validators and transformers

--- src/shared/services/question-request.service.ts
+++ src/shared/services/question-request.service.ts
@@ -24,14 +24,6 @@ export default abstract class QuestionRequestService {
             { key: 'Schleswig-Holstein', value: 15 },
             { key: 'Thüringen', value: 16 },
           ],
-          validators: [
-            {
-              isValide: (value: any) => {
-                return value !== null && value !== undefined && value !== '';
-              },
-              message: 'bitte das Feld füllen',
-            },
-          ],
         },
         description: `
           Hiermit können wir dir helfen die Programme aus deinem Bundesland für dich zu finden.
@@ -59,14 +51,6 @@ export default abstract class QuestionRequestService {
             { key: 'Venture Capital Gesellschaft', value: 12 },
             { key: 'Technologie', value: 13 },
             { key: 'Sonstiges', value: 11 },
-          ],
-          validators: [
-            {
-              isValide: (value: any) => {
-                return value !== null && value !== undefined && value !== '';
-              },
-              message: 'bitte das Feld füllen',
-            },
           ],
         },
         description: `
@@ -111,14 +95,6 @@ export default abstract class QuestionRequestService {
 
             { key: 'Sonstige', value: 25 },
           ],
-          validators: [
-            {
-              isValide: (value: any) => {
-                return value !== null && value !== undefined && value !== '';
-              },
-              message: 'bitte das Feld füllen',
-            },
-          ],
         },
         description: `
         Die Rechtsform ist der durch Gesetze zwingend vorgeschriebene rechtliche Rahmen von Gesellschaften, mit dem einige gesetzlich vorgegebene Strukturmerkmale verbunden sind und mit dem Gesellschaften am Wirtschaftsleben teilnehmen.
@@ -131,121 +107,11 @@ export default abstract class QuestionRequestService {
           key: 'age',
           unit: 'Jahre',
           placeholder: 'XX',
-          validators: [
-            {
-              isValide: (value: any) => {
-                return value !== null && value !== undefined && value !== '';
-              },
-              message: 'bitte das Feld füllen',
-            },
-          ],
         },
         description: `
           Für junge und bereits etablierte Unternehmen gibt es oft unterschiedliche Förderprogramme.
           Lass uns wissen seit wie vielen Jahren es dein Unternehmen bereits gibt
           und wir suchen für dich die passenden Angebote.
-        `,
-      },
-      {
-        title: 'Wieviele Beschäftigte hat dein Unternehmen?',
-        config: {
-          type: 'employees',
-          key: 'employees',
-          options: [],
-          unit: 'Mitarbeiter',
-          placeholder: 'XX',
-          validators: [
-            {
-              isValide: (value: any) => {
-                return value !== null && value !== undefined && value !== '';
-              },
-              message: 'bitte das Feld füllen',
-            },
-          ],
-        },
-        factorsTitle:
-          'Umrechnung von Teilzeitkräften und 450 Euro-Jobs in VZÄ für Mitarbeiter:innen:',
-        factors: [
-          {
-            time: '450 Euro-Basis',
-            value: 0.3,
-          },
-          {
-            time: 'bis 20 Stunden',
-            value: 0.5,
-          },
-          {
-            time: 'bis 30 Stunden',
-            value: 0.75,
-          },
-          {
-            time: 'über 30 Stunden',
-            value: 1,
-          },
-        ],
-        description: `
-        <b>Bitte das Ergebnis auf die nächsthöhere Zahl aufrunden
-        und als Solo-Unternehmer:in ohne Mitarbeiter:innen 0 eintragen</b>
-        `,
-      },
-      {
-        title: 'Wie groß war der Umsatz (2019)?',
-        config: {
-          type: 'select',
-          key: 'sales',
-          options: [
-            { key: '0 - 2 Million', value: 0 },
-            { key: '2 - 5 Millionen', value: 1 },
-            { key: '5 - 10 Millionen', value: 2 },
-            { key: '10 - 50 Millionen', value: 3 },
-            { key: '50 - 500 Millionen', value: 4 },
-            { key: 'mehr als 500 Millionen', value: 5 },
-          ],
-          validators: [
-            {
-              isValide: (value: any) => {
-                return value !== null && value !== undefined && value !== '';
-              },
-              message: 'bitte das Feld füllen',
-            },
-          ],
-          transform: (value: number) =>
-            [(0 + 2) / 2, (2 + 5) / 2, (5 + 10) / 2, (10 + 50) / 2, (50 + 500) / 2, 600][value] *
-            1000000,
-        },
-        description: `
-          Je nach Umsatz deines Unternehmens gibt es unterschiedliche Förderprogramme,
-          lass uns den Umsatz des letzten Jahres wissen,
-          damit wir die für dich passensten Programme finden können.
-        `,
-      },
-      {
-        title: 'Wie groß war die Bilanzsumme (2019)?',
-        config: {
-          type: 'select',
-          key: 'totalAssets',
-          options: [
-            { key: '0 - 1 Million', value: 0 },
-            { key: '1 - 2 Millionen', value: 1 },
-            { key: '2 - 10 Millionen', value: 2 },
-            { key: '10 - 20 Millionen', value: 3 },
-            { key: '20 - 43 Millionen', value: 4 },
-            { key: 'mehr als 43 Millionen', value: 5 },
-          ],
-          validators: [
-            {
-              isValide: (value: any) => {
-                return value !== null && value !== undefined && value !== '';
-              },
-              message: 'bitte das Feld füllen',
-            },
-          ],
-          transform: (value: number) =>
-            [(0 + 1) / 2, (1 + 2) / 2, (2 + 10) / 2, (10 + 20) / 2, (20 + 43) / 2, 44][value] *
-            1000000,
-        },
-        description: `
-          Sollte dein Unternehmen keine Bilanz aufstellen,<br>wähle bitte <b>"0 - 1 Million"</b>.
         `,
       },
     ];
