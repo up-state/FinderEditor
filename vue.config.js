f87b9cf4c650972705e087f47320909a927e13d6
Dennis Breitling <breitling@posteo.de>
2021-03-20 18:45:51 +0100

    probably fixes the deployment

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
+  // GH pages are served from a sub-path, which we have to accomodate for
+  publicPath: process.env.NODE_ENV === 'production' ? '/FinderEditor/' : '/',
   lintOnSave: false,
 };
