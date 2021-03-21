edd559532e2a1aaba0455e8a0f71a1dcb6e6f7c8
Dennis Breitling <breitling@posteo.de>
2021-03-21 17:24:46 +0100

    add result mock texts

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -2,14 +2,24 @@
   <div class="results max-screen">
     <h1 style="text-align: left">Resultateseite</h1>
     <h4>Auf Basis Ihrer Eingaben haben wir folgende Angebote für Sie.</h4>
-    <el-collapse id="results-list" v-for="(result, index) in resultItems" :key="index" accordion>
+    <el-collapse id="results-list" v-for="(result, index) in resultItems" :key="index">
       <el-collapse-item>
-        <template slot="title" style="margin-left: 5px">
-          <h4 class="editor__header">{{ result.title }}</h4>
+        <template slot="title" id="result-header" style="background: #dfefff">
+          <div style="">
+            <i class="el-icon-notebook-2"></i>
+            <h4 style="display: inline; margin-left: 10px">{{ result.title }}</h4>
+          </div>
         </template>
         <div class="collapse-content">
           <div style="background: #fff400; width: 5px" />
-          <div>{{ result.description }}</div>
+          <div>
+            <h4 class="result-title-sub">{{ result.titleA }}</h4>
+            <div>{{ result.descriptionA }}</div>
+          </div>
+          <div>
+            <h4 class="result-title-sub">{{ result.titleB }}</h4>
+            <div>{{ result.descriptionB }}</div>
+          </div>
         </div>
       </el-collapse-item>
     </el-collapse>
@@ -20,7 +30,6 @@
 <script lang="ts">
 // @ is an alias to /src
 import { Component, Vue } from 'vue-property-decorator';
-import { FinderService } from '../shared/services/finder.service';
 
 @Component({
   components: {},
@@ -33,15 +42,30 @@ export default class Results extends Vue {
   public resultItems: any[] = [
     {
       title: 'Wohnen',
-      description: 'Wohnberechtigungsschein beantragen',
+      titleA: 'Wohnberechtigungsschein beantragen',
+      descriptionA:
+        'In Ihrer Lebenslage können Sie beim zuständigen Amt einen Wohnberechtigungsschein beantragen.',
+      descriptionB:
+        'In Ihrer Lebenslage können Sie beim zuständigen Amt einen Zuschuss zum Umzug beantragen.',
+      titleB: 'Zuschuss zum Umzug beantragen',
     },
     {
       title: 'Familie',
-      description: 'Wohnberechtigungsschein beantragen',
+      titleA: 'Lorem ipsum dolor sit amet',
+      descriptionA:
+        'Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+      titleB: 'Lorem ipsum dolor sit amet',
+      descriptionB:
+        'Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
     },
     {
       title: 'Arbeit',
-      description: 'Wohnberechtigungsschein beantragen',
+      titleA: 'Lorem ipsum dolor sit amet',
+      descriptionA:
+        'Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+      titleB: 'Lorem ipsum dolor sit amet',
+      descriptionB:
+        'Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
     },
   ];
 
@@ -56,5 +80,15 @@ export default class Results extends Vue {
 <style scoped lang="scss">
 #results-list {
   width: 60em;
+}
+.collapse-content {
+  padding: 10px;
+}
+.el-collapse-item__header {
+  background: #dfefff !important;
+  margin-left: 5px !important;
+}
+.result-title-sub {
+  margin-bottom: 0;
 }
 </style>
