f8cc5fe236e971e5da5396d015f25960e073ce06
Dennis Breitling <breitling@posteo.de>
2021-03-21 12:50:12 +0100

    adds login button to login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,32 +2,20 @@
   <div class="home container-xs screen">
     <h1>{{ $router.currentRoute.meta.title }}</h1>
     <div class="login__container">
-      <label for="mail" class="login__label"
-        >E-Mail-Adresse/Benutzername</label
-      >
-      <input
-        id="mail"
-        type="text"
-        class="login__input"
-        value="test@meinantrag.de"
-      />
+      <label for="mail" class="login__label">E-Mail-Adresse/Benutzername</label>
+      <input id="mail" type="text" class="login__input" value="test@meinantrag.de" />
     </div>
     <div class="login__container">
       <label for="password" class="login__label">Passwort</label>
-      <input
-        id="password"
-        type="password"
-        class="login__input"
-        value="FakePSW"
-      />
+      <input id="password" type="password" class="login__input" value="FakePSW" />
     </div>
+    <el-button type="primary" v-on:click="toEditor()">Login</el-button>
   </div>
 </template>
 
 <script lang="ts">
-import { ButtonConfig } from '../components/NavFooter/ButtonConfig.class';
 import Progress from '../components/Progress.vue';
-import { Component, Vue, Emit } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 
 @Component({
   components: {
@@ -35,33 +23,16 @@ import { Component, Vue, Emit } from 'vue-property-decorator';
   },
 })
 export default class Login extends Vue {
-  public buttonsConfig: ButtonConfig[] = [
-    new ButtonConfig('Login', false, () => {
-      this.toEditor();
-    }),
-  ];
-
-  @Emit('updateStatus')
-  updateStatus(): ButtonConfig[] {
-    return this.buttonsConfig;
-  }
-
   public toEditor(): void {
     this.$router.push({
       path: '/editor',
     });
   }
-
-  mounted() {
-    this.updateStatus();
-  }
 }
 </script>
 <style lang="scss">
-
 .login__container {
-    margin: 0rem auto 1rem auto;
-
+  margin: 0rem auto 1rem auto;
 }
 .login__label {
   display: block;
