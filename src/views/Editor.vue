7cea93c854c68c4dee2d276afd52a06baa59b79d
Fabian Wolff <Fabian_Wolff1@yahoo.de>
2021-03-21 14:35:29 +0100

    bg color

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -146,8 +146,7 @@ export default class Start extends Vue {
 
 .box {
   background-color: white;
-  box-shadow: rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px,
-    rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
   border-radius: 10px;
   padding: 2rem;
   min-height: 60vh;
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -58,8 +58,7 @@ export default class Login extends Vue {
   margin: auto;
   width: 20em !important;
   background-color: white;
-  box-shadow: rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px,
-    rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
   border-radius: 10px;
   padding: 2rem;
 }
