f587721e5b60411461114c5402ce75bf31828834
Fabian Wolff <Fabian_Wolff1@yahoo.de>
2021-03-21 16:59:54 +0100

    added download btn

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -4,9 +4,11 @@
       <el-button disabled><i class="el-icon-back"></i>Familie</el-button>
       <el-badge :value="questions.length" class="item">
         <el-button style="margin-left: 10px" type="primary" v-on:click="toFinder()"
-          ><i class="el-icon-right" />Zum Finder</el-button
+          ><i class="el-icon-right" />Vorschau</el-button
         >
       </el-badge>
+      <el-button style="margin-left: 10px; background: white; box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);" icon="el-icon-download
+"></el-button>
     </nav>
 
     <div class="editor-main">
